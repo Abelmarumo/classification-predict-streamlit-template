923b5a34d92f00e69f06e4c4baf2a8864cb406d1
Abelmarumo <marumojonell@gmail.com>
2020-06-29 04:52:06 +0200

    deployment

--- base_app.py
+++ base_app.py
@@ -24,6 +24,8 @@
 # Streamlit dependencies
 import streamlit as st
 import joblib,os
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Data dependencies
 import pandas as pd
@@ -46,38 +48,92 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
-	selection = st.sidebar.selectbox("Choose Option", options)
+	options = ["Classifier Page", "General Information Page"]
+	selection = st.sidebar.selectbox("Choose Page", options)
 
-	# Building out the "Information" page
-	if selection == "Information":
-		st.info("General Information")
-		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
+	
+	
+	
+	# Building out the General inforation page
+	if selection=="General Information Page":
+		sub_options1= ["Bar graph", "Pie chart"]
+		sub_selection1 = st.sidebar.selectbox("General information", sub_options1)
+		if sub_selection1 == "Bar graph":
+			raw['sentiment'].value_counts().plot(kind='bar')
+			st.pyplot()
+			st.info("General Information")
+			# You can read a markdown file from supporting resources folder
+			st.markdown("Some information here")
 
-		st.subheader("Raw Twitter data and label")
-		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
-			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+			st.subheader("Raw Twitter data and label")
+			if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+				st.write(raw[['sentiment', 'message']]) # will write the df to the page
+		else:
+			plt.pie(x=raw['sentiment'].value_counts().values,labels=raw['sentiment'].value_counts().index)
+			st.pyplot()
+			st.info("General Information")
+			# You can read a markdown file from supporting resources folder
+			st.markdown("Some information here")
 
-	# Building out the predication page
-	if selection == "Prediction":
-		st.info("Prediction with ML Models")
-		# Creating a text box for user input
-		tweet_text = st.text_area("Enter Text","Type Here")
+			st.subheader("Raw Twitter data and label")
+			if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+				st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
 
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
+	if selection=="Classifier Page":
+		sub_options2= ["Multinomial Logistic Regression", "LinearSVC"]
+		sub_selection2 = st.sidebar.selectbox("Choose classifier", sub_options2)
+		if sub_selection2 == "Multinomial Logistic Regression":
+			st.info("Prediction with "+sub_selection2)
+			# Creating a text box for user input
+			tweet_text = st.text_area("Enter Text","Type Here")
 
+			if st.button("Classify"):
+				# Transforming user input with vectorizer
+				#vect_text = tweet_cv.transform([tweet_text]).toarray()
+				# Load your .pkl file with the model of your choice + make predictions
+				# Try loading in multiple models to give the user a choice
+				predictor = joblib.load(open(os.path.join("resources/multinomial_logistic.plk"),"rb"))
+				prediction = predictor.predict([tweet_text])
+
+				# When model has successfully run, will print prediction
+				# You can use a dictionary or similar structure to make this output
+				# more human interpretable.
+				if prediction[0]==2:
+					st.success("Text Categorized as News")
+				elif prediction[0]==1:	
+					st.success("Text Categorized as Pro")
+				elif prediction[0]==0:	
+					st.success("Text Categorized as Neutral")
+				elif prediction[0]==-1:	
+					st.success("Text Categorized as Anti")
+###
+		if sub_selection2 == "LinearSVC":
+			st.info("Prediction with "+sub_selection2)
+			# Creating a text box for user input
+			tweet_text = st.text_area("Enter Text","Type Here")
+
+			if st.button("Classify"):
+				# Transforming user input with vectorizer
+				#vect_text = tweet_cv.transform([tweet_text]).toarray()
+				# Load your .pkl file with the model of your choice + make predictions
+				# Try loading in multiple models to give the user a choice
+				predictor = joblib.load(open(os.path.join("resources/LinearSVC.plk"),"rb"))
+				prediction = predictor.predict([tweet_text])
+
+				# When model has successfully run, will print prediction
+				# You can use a dictionary or similar structure to make this output
+				# more human interpretable.
+				if prediction[0]==2:
+					st.success("Text Categorized as News")
+				elif prediction[0]==1:	
+					st.success("Text Categorized as Pro")
+				elif prediction[0]==0:	
+					st.success("Text Categorized as Neutral")
+				elif prediction[0]==-1:	
+					st.success("Text Categorized as Anti")
+	
+###
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
 	main()
---
+++ streamApp.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import joblib,os
+import spacy
+from wordcloud import WordCloud
+from PIL import Image
+import matplotlib.pyplot as plt
+
+nlp = spacy.load('en')
+
+
+
+def get_keys(value,my_dic):
+    for key,val in my_dic.items():
+        if val==value:
+            return key
+def main():
+    """tweet classifier"""
+    st.title("Tweet Classifier")
+    st.subheader("NLP and ML App with streamlit")
+
+    activities =["Prediction","NLP"]
+    choice = st.sidebar.selectbox("Choose Activity",activities)
+
+    if choice=='Prediction':
+        st.info("Prediction with ML")
+        tweet_text = st.text_area("Enter Text","Type Here")
+        all_ml_modules = ["MLR","LSVC"]
+        model_choice = st.selectbox("Choose the ML model",all_ml_modules)
+        labels = {'News':2,'Pro':1,'Neutral':0,'Anti':-1}
+        if st.button("Classify"):
+            st.text("Original tweet ::\n{}".format(tweet_text))
+            if model_choice=="MLR":
+                predictor = joblib.load(open(os.path.join("resources/multinomial_logistic.plk"),"rb"))
+                prediction = predictor.predict([tweet_text])
+                ##st.write(prediction[0])
+                st.success(f"The tweet is categorized as {get_keys(prediction[0],labels)}")
+            if model_choice=="LSVC":
+                predictor = joblib.load(open(os.path.join("resources/LinearSVC.plk"),"rb"))
+                prediction = predictor.predict([tweet_text])
+                ##st.write(prediction[0])
+                st.success(f"The tweet is categorized as {get_keys(prediction[0],labels)}")
+
+    if choice=='NLP':
+        st.info("Natural Language Processing")
+        tweet_text = st.text_area("Enter Text","Type Here")
+        nlp_task = ["Tokenization","NER","Lemmatization","POS"]
+        task_choice = st.selectbox("Choose NLP Task",nlp_task)
+        if st.button('Analysis'):
+            st.info("Original tweet ::\n{}".format(tweet_text))
+
+            doc = nlp(tweet_text)
+            if task_choice =="Tokenization":
+                tokens = [t.text for t in doc]
+
+            elif task_choice =="NER":
+                tokens = [(ent.text,ent.label_) for ent in doc.ents]
+
+            elif task_choice =="Lemmatization":
+                tokens = [f"'Token':{t.text},'Lemma':{t.lemma_}" for t in doc]
+            elif task_choice =="POS":
+                tokens = [f"'Token':{t.text},'POS':{t.pos_},'Dependency':{t.dep_}" for t in doc]
+            st.json(tokens)
+        if st.button("Wordcloud"):
+            wordcloud  = WordCloud().generate(tweet_text)
+            plt.imshow(wordcloud)
+            plt.axis('off')
+            st.pyplot()
+
+
+
+if __name__ == "__main__":
+    main()
